bacd915195ca9e9da04c443a8d235692df097d17
oops <oops@2a597754-026b-448c-8dd0-9ec5e4436b14>
2012-09-29 16:20:17 +0000

    add proset

--- trunk/EDB/EDB_SQLITE3.php
+++ trunk/EDB/EDB_SQLITE3.php
@@ -12,7 +12,7 @@
  * @author      JoungKyun.Kim <http://oops.org>
  * @copyright   (c) 2012 JoungKyun.Kim
  * @license     BSD License
- * @version     $Id: EDB_Common.php 4 2012-08-31 19:14:39Z oops $
+ * @version     $Id$
  * @link        http://pear.oops.org/package/EDB
  * @filesource
  */
